eb1b331ed2eaf0cdc7a1e01a9d44444d00dac556
damianfral <huevofritopamojarpan@gmail.com>
2026-06-19 01:21:30 +0000

    Use ifoldFree in interpretESP.

--- src/ESPBunker/Interpreter.hs
+++ src/ESPBunker/Interpreter.hs
@@ -5,7 +5,9 @@
 
 module ESPBunker.Interpreter where
 
+import Control.Monad.Indexed (IxFunctor (..))
 import Control.Monad.Indexed.Free (IxFree (..))
+import Control.Monad.Writer (Writer, execWriter, tell)
 import Data.Aeson
 import Data.Aeson.Casing (snakeCase)
 import Data.Proxy
@@ -21,204 +23,213 @@ import Relude hiding (State, natVal, return)
 
 --------------------------------------------------------------------------------
 
-interpretESP ::
-  forall board board' boardName boardNames boardGPIOs boardADCPins boardLEDCPins.
-  ( board ~ Board boardName boardNames boardGPIOs boardADCPins boardLEDCPins,
-    KnownSymbol boardName
-  ) =>
-  ESPM board board' () -> [Node]
-interpretESP (Pure _) = []
-interpretESP (Free espf) = case espf of
-  MkESPHome @name options next ->
-    let n = symbolVal (Proxy @name)
-        espHomeOpts = ["name" .= n] <> toKeyMap options
-        espHomeNode = NodeObject "esphome" espHomeOpts
-     in espHomeNode : interpretESP next
-  MkBoard next ->
-    let boardName = symbolVal (Proxy @boardName)
-        boardSubnode =
-          [ "board" .= boardName,
-            "framework"
-              .= object
-                [("type", toJSON FrameworkArduino), ("version", toJSON Latest)]
-          ]
-        yamlNode = NodeObject "esp32" boardSubnode
-     in yamlNode : interpretESP next
-  MkLogger next ->
-    let espHomeNode = NodeObject "logger" mempty
-     in espHomeNode : interpretESP next
-  MkSwitch @name @platform @pin opts next ->
-    let n = symbolVal (Proxy @name)
-        platform = symbolVal (Proxy @(PlatformToSymbol platform))
-        yamlNode =
-          NodeArray
-            "switch"
-            [ [ "platform" .= platform,
-                "pin" .= pinToText @pin,
-                "name" .= n,
-                "id" .= snakeCase n
-              ]
-                <> toKeyMap opts
-            ]
-     in yamlNode : interpretESP next
-  MkCover @name @platform opts next ->
-    let n = symbolVal (Proxy @name)
-        platform = symbolVal (Proxy @(PlatformToSymbol platform))
-        yamlNode =
-          NodeArray
-            "cover"
-            [ ["platform" .= platform, "name" .= n, "id" .= snakeCase n]
-                <> toKeyMap opts
-            ]
-     in yamlNode : interpretESP next
-  MkButton @name @pin options next -> do
-    let n = symbolVal (Proxy @name)
-        buttonNode =
-          NodeArray
-            "button"
-            [[("platform", "template"), "name" .= n, "id" .= snakeCase n]]
-        baseOpts =
-          [ "platform" .= String "gpio",
-            "pin" .= pinToText @pin,
-            "name" .= n,
-            "id" .= (snakeCase n <> "_binary_sensor")
-          ]
-        allOpts = fold @[] [baseOpts, toKeyMap options]
-        binarySensorNode = NodeArray "binary_sensor" (allOpts :| [])
-    buttonNode : binarySensorNode : interpretESP next
-  MkOutput @name @platform @pin opts next ->
-    let n = symbolVal (Proxy @name)
-        platform = symbolVal (Proxy @(PlatformToSymbol platform))
-        yamlNode =
-          NodeArray
-            "output"
-            [ [ "platform" .= platform,
-                "id" .= snakeCase n,
-                "pin" .= pinToText @pin
-              ]
-                <> toKeyMap opts
-            ]
-     in yamlNode : interpretESP next
-  MkNumber @name options next ->
-    let n = symbolVal (Proxy @name)
-        opts =
-          [("platform", "template"), "name" .= n, "id" .= snakeCase n]
-            <> toKeyMap options
-        yamlNode = NodeArray "number" (opts :| [])
-     in yamlNode : interpretESP next
-  MkSelect @name options next ->
-    let n = symbolVal (Proxy @name)
-        opts =
-          [("platform", "template"), "name" .= n, "id" .= snakeCase n]
-            <> toKeyMap options
-        yamlNode = NodeArray "select" (opts :| [])
-     in yamlNode : interpretESP next
-  MkSensor @name @platform @pin options platformOptions next ->
-    let n = symbolVal (Proxy @name)
-        platform = symbolVal (Proxy @(PlatformToSymbol platform))
-        opts =
-          [ "platform" .= platform,
-            "name" .= n,
-            "id" .= snakeCase n,
-            "pin" .= natVal (Proxy @pin)
-          ]
-            <> toKeyMap options
-            <> toKeyMap platformOptions
-        yamlNode = NodeArray "sensor" (opts :| [])
-     in yamlNode : interpretESP next
-  MkTextSensor @name next ->
-    let n = symbolVal (Proxy @name)
-        yamlNode =
-          NodeArray
-            "text_sensor"
-            [[("platform", "template"), "name" .= n, "id" .= snakeCase n]]
-     in yamlNode : interpretESP next
-  MkBinarySensor @name @platform @pin options platformOptions next ->
-    let n = symbolVal (Proxy @name)
-        platform = symbolVal (Proxy @(PlatformToSymbol platform))
-        baseOpts = ["platform" .= platform, "name" .= n, "id" .= snakeCase n]
-        pinModeOpts = case binarySensorPinMode options of
-          Nothing -> ["pin" .= pinToText @pin]
-          Just mode ->
-            let pinObj =
-                  [ "number" .= pinToText @pin,
-                    "mode"
-                      .= object
-                        ( catMaybes
-                            [ do
-                                guard $ pinModeInput mode
-                                Just ("input", toJSON True),
-                              do
-                                guard $ pinModeOutput mode
-                                Just ("output", toJSON True),
-                              do
-                                guard $ pinModeOpenDrain mode
-                                Just ("open_drain", toJSON True),
-                              do
-                                guard $ pinModePullUp mode
-                                Just ("pullup", toJSON True),
-                              do
-                                guard $ pinModePullDown mode
-                                Just ("pulldown", toJSON True)
-                            ]
-                        )
-                  ]
-             in ["pin" .= object pinObj]
-        allOpts =
-          fold @[]
-            [baseOpts, pinModeOpts, toKeyMap options, toKeyMap platformOptions]
+ifoldFree ::
+  (IxFunctor f, Monad n) =>
+  (forall i j. f i j (n ()) -> n ()) -> IxFree f k m () -> n ()
+ifoldFree _ (Pure _) = pure ()
+ifoldFree alg (Free fx) = alg $ imap (ifoldFree alg) fx
 
-        yamlNode = NodeArray "binary_sensor" (allOpts :| [])
-     in [yamlNode] <> interpretESP next
-  MkLight @name @platform options platformOptions next ->
-    let n = symbolVal $ Proxy @name
-        platform = symbolVal $ Proxy @(PlatformToSymbol platform)
-        yamlNode =
-          NodeArray
-            "light"
-            [ ["platform" .= platform, "name" .= n, "id" .= snakeCase n]
-                <> toKeyMap options
-                <> toKeyMap platformOptions
+interpretESP ::
+  forall board' boardName boardNames boardGPIOs boardADCPins boardLEDCPins.
+  (KnownSymbol boardName) =>
+  ESPM
+    (Board boardName boardNames boardGPIOs boardADCPins boardLEDCPins)
+    board'
+    () ->
+  [Node]
+interpretESP = execWriter . ifoldFree go
+  where
+    go :: forall i j. ESPF i j (Writer [Node] ()) -> Writer [Node] ()
+    go (MkESPHome @name options next) = do
+      let n = symbolVal (Proxy @name)
+          espHomeOpts = ["name" .= n] <> toKeyMap options
+          espHomeNode = NodeObject "esphome" espHomeOpts
+      tell [espHomeNode] >> next
+    go (MkBoard next) = do
+      let boardName = symbolVal (Proxy @boardName)
+          boardSubnode =
+            [ "board" .= boardName,
+              "framework"
+                .= object
+                  [("type", toJSON FrameworkArduino), ("version", toJSON Latest)]
             ]
-     in [yamlNode] <> interpretESP next
-  MkScript @name action next ->
-    let n = symbolVal $ Proxy @name
-        yamlNode =
-          NodeArray
-            "script"
-            [["id" .= snakeCase n, "then" .= interpretAction action]]
-     in yamlNode : interpretESP next
-  MkWifi options next ->
-    let wifiNode = NodeObject "wifi" (toKeyMap options)
-     in wifiNode : interpretESP next
-  MkAPI options next ->
-    let apiNode = NodeObject "api" (toKeyMap options)
-     in apiNode : interpretESP next
-  MkOTA options next ->
-    case nonEmpty (toKeyMap <$> options) of
-      Just otas -> NodeArray "ota" otas : interpretESP next
-      Nothing -> interpretESP next
-  MkWebServer options next ->
-    let webServerNode = NodeObject "web_server" (toKeyMap options)
-     in webServerNode : interpretESP next
-  MkI2C @name options next ->
-    let n = symbolVal (Proxy @name)
-        i2cNode = NodeArray "i2c" [["id" .= n] <> toKeyMap options]
-     in i2cNode : interpretESP next
-  MkPN532I2C @name options next ->
-    let n = symbolVal (Proxy @name)
-        allFields = ["id" .= snakeCase n] <> toKeyMap options
-        yamlNode = NodeArray "pn532_i2c" (allFields :| [])
-     in yamlNode : interpretESP next
-  MkInterval @name options next ->
-    let n = symbolVal (Proxy @name)
-        intervalNode =
-          NodeArray
-            "interval"
-            [ ["id" .= maybe (snakeCase n) toString (intervalId options)]
-                <> toKeyMap options
+          yamlNode = NodeObject "esp32" boardSubnode
+      tell [yamlNode] >> next
+    go (MkLogger next) = do
+      let espHomeNode = NodeObject "logger" mempty
+      tell [espHomeNode] >> next
+    go (MkSwitch @name @platform @pin opts next) = do
+      let n = symbolVal (Proxy @name)
+          platform = symbolVal (Proxy @(PlatformToSymbol platform))
+          yamlNode =
+            NodeArray
+              "switch"
+              [ [ "platform" .= platform,
+                  "pin" .= pinToText @pin,
+                  "name" .= n,
+                  "id" .= snakeCase n
+                ]
+                  <> toKeyMap opts
+              ]
+      tell [yamlNode] >> next
+    go (MkCover @name @platform opts next) = do
+      let n = symbolVal (Proxy @name)
+          platform = symbolVal (Proxy @(PlatformToSymbol platform))
+          yamlNode =
+            NodeArray
+              "cover"
+              [ ["platform" .= platform, "name" .= n, "id" .= snakeCase n]
+                  <> toKeyMap opts
+              ]
+      tell [yamlNode] >> next
+    go (MkButton @name @pin options next) = do
+      let n = symbolVal (Proxy @name)
+          buttonNode =
+            NodeArray
+              "button"
+              [[("platform", "template"), "name" .= n, "id" .= snakeCase n]]
+          baseOpts =
+            [ "platform" .= String "gpio",
+              "pin" .= pinToText @pin,
+              "name" .= n,
+              "id" .= (snakeCase n <> "_binary_sensor")
             ]
-     in intervalNode : interpretESP next
+          allOpts = fold @[] [baseOpts, toKeyMap options]
+          binarySensorNode = NodeArray "binary_sensor" (allOpts :| [])
+      tell [buttonNode, binarySensorNode] >> next
+    go (MkOutput @name @platform @pin opts next) = do
+      let n = symbolVal (Proxy @name)
+          platform = symbolVal (Proxy @(PlatformToSymbol platform))
+          yamlNode =
+            NodeArray
+              "output"
+              [ [ "platform" .= platform,
+                  "id" .= snakeCase n,
+                  "pin" .= pinToText @pin
+                ]
+                  <> toKeyMap opts
+              ]
+      tell [yamlNode] >> next
+    go (MkNumber @name options next) = do
+      let n = symbolVal (Proxy @name)
+          opts =
+            [("platform", "template"), "name" .= n, "id" .= snakeCase n]
+              <> toKeyMap options
+          yamlNode = NodeArray "number" (opts :| [])
+      tell [yamlNode] >> next
+    go (MkSelect @name options next) = do
+      let n = symbolVal (Proxy @name)
+          opts =
+            [("platform", "template"), "name" .= n, "id" .= snakeCase n]
+              <> toKeyMap options
+          yamlNode = NodeArray "select" (opts :| [])
+      tell [yamlNode] >> next
+    go (MkSensor @name @platform @pin options platformOptions next) = do
+      let n = symbolVal (Proxy @name)
+          platform = symbolVal (Proxy @(PlatformToSymbol platform))
+          opts =
+            [ "platform" .= platform,
+              "name" .= n,
+              "id" .= snakeCase n,
+              "pin" .= natVal (Proxy @pin)
+            ]
+              <> toKeyMap options
+              <> toKeyMap platformOptions
+          yamlNode = NodeArray "sensor" (opts :| [])
+      tell [yamlNode] >> next
+    go (MkTextSensor @name next) = do
+      let n = symbolVal (Proxy @name)
+          yamlNode =
+            NodeArray
+              "text_sensor"
+              [[("platform", "template"), "name" .= n, "id" .= snakeCase n]]
+      tell [yamlNode] >> next
+    go (MkBinarySensor @name @platform @pin options platformOptions next) = do
+      let n = symbolVal (Proxy @name)
+          platform = symbolVal (Proxy @(PlatformToSymbol platform))
+          baseOpts = ["platform" .= platform, "name" .= n, "id" .= snakeCase n]
+          pinModeOpts = case binarySensorPinMode options of
+            Nothing -> ["pin" .= pinToText @pin]
+            Just mode ->
+              let pinObj =
+                    [ "number" .= pinToText @pin,
+                      "mode"
+                        .= object
+                          ( catMaybes
+                              [ do
+                                  guard $ pinModeInput mode
+                                  Just ("input", toJSON True),
+                                do
+                                  guard $ pinModeOutput mode
+                                  Just ("output", toJSON True),
+                                do
+                                  guard $ pinModeOpenDrain mode
+                                  Just ("open_drain", toJSON True),
+                                do
+                                  guard $ pinModePullUp mode
+                                  Just ("pullup", toJSON True),
+                                do
+                                  guard $ pinModePullDown mode
+                                  Just ("pulldown", toJSON True)
+                              ]
+                          )
+                    ]
+               in ["pin" .= object pinObj]
+          allOpts =
+            fold @[]
+              [baseOpts, pinModeOpts, toKeyMap options, toKeyMap platformOptions]
+          yamlNode = NodeArray "binary_sensor" (allOpts :| [])
+      tell [yamlNode] >> next
+    go (MkLight @name @platform options platformOptions next) = do
+      let n = symbolVal $ Proxy @name
+          platform = symbolVal $ Proxy @(PlatformToSymbol platform)
+          yamlNode =
+            NodeArray
+              "light"
+              [ ["platform" .= platform, "name" .= n, "id" .= snakeCase n]
+                  <> toKeyMap options
+                  <> toKeyMap platformOptions
+              ]
+      tell [yamlNode] >> next
+    go (MkScript @name action next) = do
+      let n = symbolVal $ Proxy @name
+          yamlNode =
+            NodeArray
+              "script"
+              [["id" .= snakeCase n, "then" .= interpretAction action]]
+      tell [yamlNode] >> next
+    go (MkWifi options next) = do
+      let wifiNode = NodeObject "wifi" (toKeyMap options)
+      tell [wifiNode] >> next
+    go (MkAPI options next) = do
+      let apiNode = NodeObject "api" (toKeyMap options)
+      tell [apiNode] >> next
+    go (MkOTA options next) = do
+      case nonEmpty (toKeyMap <$> options) of
+        Just otas -> tell [NodeArray "ota" otas]
+        Nothing -> pure ()
+      next
+    go (MkWebServer options next) = do
+      let webServerNode = NodeObject "web_server" (toKeyMap options)
+      tell [webServerNode] >> next
+    go (MkI2C @name options next) = do
+      let n = symbolVal (Proxy @name)
+          i2cNode = NodeArray "i2c" [["id" .= n] <> toKeyMap options]
+      tell [i2cNode] >> next
+    go (MkPN532I2C @name options next) = do
+      let n = symbolVal (Proxy @name)
+          allFields = ["id" .= snakeCase n] <> toKeyMap options
+          yamlNode = NodeArray "pn532_i2c" (allFields :| [])
+      tell [yamlNode] >> next
+    go (MkInterval @name options next) = do
+      let n = symbolVal (Proxy @name)
+          intervalNode =
+            NodeArray
+              "interval"
+              [ ["id" .= maybe (snakeCase n) toString (intervalId options)]
+                  <> toKeyMap options
+              ]
+      tell [intervalNode] >> next
 
 --------------------------------------------------------------------------------
 
